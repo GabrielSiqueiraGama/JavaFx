1b6ccbdb55d7ab7db732539a214fc7a11c0b4b52
GabrielSiqueiraGama <siqueiragamagabriel@gmail.com>
2024-04-22 23:30:53 -0300

    feat: :art: Troca de telas ao click

--- src/layout/AppLayout.java
+++ src/layout/AppLayout.java
@@ -24,6 +24,7 @@ public class AppLayout extends Application{
 		raiz = new TesteBorderPane();
 		raiz = new TesteFlowPane();
 		raiz = new TesteGrid();
+		raiz = new TesteStackPane();
 		Scene principal = new Scene(raiz, 800, 600);
 		janela = primaryStage;
 		janela.setTitle("Gerenciadores de Layout");
